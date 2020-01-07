72147ff26d8b47edc14639c691c5e701702fd0a4
William Hunt <bill.hunt@clearavenue.com>
2020-01-07 11:34:02 -0500

    added pom and docker improvements and convert to jpa

--- src/main/java/com/clearavenue/fdadi/repository/MedicationRepository.java
+++ src/main/java/com/clearavenue/fdadi/repository/MedicationRepository.java
@@ -2,14 +2,13 @@ package com.clearavenue.fdadi.repository;
 
 import java.util.Optional;
 
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.clearavenue.fdadi.model.Medication;
 
 @Repository
-public interface MedicationRepository extends MongoRepository<Medication, ObjectId> {
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
 
 	Optional<Medication> findByMedicationName(String med);
 }
