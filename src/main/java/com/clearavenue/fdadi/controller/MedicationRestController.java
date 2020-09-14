440f0d4892c4f8cf123a75d592776bc2549120f6
William Hunt <bill.hunt@clearavenue.com>
2020-09-14 16:05:16 -0400

    Updated spring version

--- src/main/java/com/clearavenue/fdadi/controller/MedicationRestController.java
+++ src/main/java/com/clearavenue/fdadi/controller/MedicationRestController.java
@@ -1,81 +1,69 @@
 package com.clearavenue.fdadi.controller;
 
+import com.clearavenue.fdadi.model.*;
+import com.clearavenue.fdadi.service.ApiService;
+import com.clearavenue.fdadi.service.MedicationService;
+import com.clearavenue.fdadi.service.PharmClassService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.clearavenue.fdadi.model.AllMedicationsResult;
-import com.clearavenue.fdadi.model.AllPharmClassesResult;
-import com.clearavenue.fdadi.model.LabelResult;
-import com.clearavenue.fdadi.model.Medication;
-import com.clearavenue.fdadi.model.MedicationDetailsResult;
-import com.clearavenue.fdadi.model.MedicationResult;
-import com.clearavenue.fdadi.model.MedicationsResult;
-import com.clearavenue.fdadi.model.Meta;
-import com.clearavenue.fdadi.service.ApiService;
-import com.clearavenue.fdadi.service.MedicationService;
-import com.clearavenue.fdadi.service.PharmClassService;
-
 @RestController
+@RequiredArgsConstructor
 public class MedicationRestController {
 
-	@Autowired
-	MedicationService medService;
+    private final MedicationService medService;
+    private final PharmClassService pharmService;
+    private final ApiService api;
 
-	@Autowired
-	PharmClassService pharmService;
+    @GetMapping("/allMedications")
+    public AllMedicationsResult getAllMedications() {
+        return AllMedicationsResult.builder().medications(medService.findAll()).build();
+    }
 
-	@Autowired
-	ApiService api;
+    @GetMapping("/medication/{medicationName}")
+    public MedicationResult getMedication(@PathVariable final String medicationName) {
+        return MedicationResult.builder().medication(medService.findByMedicationName(medicationName)).build();
+    }
 
-	@GetMapping("/allMedications")
-	public AllMedicationsResult getAllMedications() {
-		return AllMedicationsResult.builder().medications(medService.findAll()).build();
-	}
+    @GetMapping("/medicationDetails/{medicationName}")
+    public MedicationDetailsResult getMedicationDetails(@PathVariable final String medicationName) {
+        LabelResult result = LabelResult.builder().meta(Meta.builder().disclaimer("No Details Found").build()).build();
 
-	@GetMapping("/medication/{medicationName}")
-	public MedicationResult getMedication(@PathVariable final String medicationName) {
-		return MedicationResult.builder().medication(medService.findByMedicationName(medicationName)).build();
-	}
+        final Optional<LabelResult> label = api.getLabel(medicationName);
+        if (label.isPresent()) {
+            result = label.get();
+        }
 
-	@GetMapping("/medicationDetails/{medicationName}")
-	public MedicationDetailsResult getMedicationDetails(@PathVariable final String medicationName) {
-		LabelResult result = LabelResult.builder().meta(Meta.builder().disclaimer("No Details Found").build()).build();
+        return MedicationDetailsResult.builder().labelResult(result).build();
+    }
 
-		final Optional<LabelResult> label = api.getLabel(medicationName);
-		if (label.isPresent()) {
-			result = label.get();
-		}
+    @GetMapping("/allPharmClasses")
+    public AllPharmClassesResult getAllPharmClasses() {
+        return AllPharmClassesResult.builder().pharmClasses(pharmService.findAll()).build();
+    }
 
-		return MedicationDetailsResult.builder().labelResult(result).build();
-	}
+    @GetMapping("/medicationsByPharmClass/{pharmClassName}")
+    public MedicationsResult getMedicationsByPharmClass(@PathVariable final String pharmClassName) {
+        final List<Medication> medicationsList = new ArrayList<>();
 
-	@GetMapping("/allPharmClasses")
-	public AllPharmClassesResult getAllPharmClasses() {
-		return AllPharmClassesResult.builder().pharmClasses(pharmService.findAll()).build();
-	}
+        final Optional<LabelResult> label = api.getLabelsByPharmClass(pharmClassName);
+        if (label.isPresent()) {
+            final LabelResult result = label.get();
+            result.results.forEach(l -> {
+                l.getOpenfda().getBrandName().stream().map(bn -> Medication.builder().medicationName(bn.toUpperCase(Locale.getDefault())).build()).forEach(medicationsList::add);
+                l.getOpenfda().getGenericName().stream().map(gn -> Medication.builder().medicationName(gn.toUpperCase(Locale.getDefault())).build()).forEach(medicationsList::add);
+            });
+        }
 
-	@GetMapping("/medicationsByPharmClass/{pharmClassName}")
-	public MedicationsResult getMedicationsByPharmClass(@PathVariable final String pharmClassName) {
-		final List<Medication> medicationsList = new ArrayList<>();
-
-		final Optional<LabelResult> label = api.getLabelsByPharmClass(pharmClassName);
-		if (label.isPresent()) {
-			final LabelResult result = label.get();
-			result.results.forEach(l -> {
-				l.getOpenfda().getBrandName().stream().map(bn -> Medication.builder().medicationName(bn.toUpperCase(Locale.getDefault())).build()).forEach(medicationsList::add);
-				l.getOpenfda().getGenericName().stream().map(gn -> Medication.builder().medicationName(gn.toUpperCase(Locale.getDefault())).build()).forEach(medicationsList::add);
-			});
-		}
-
-		return MedicationsResult.builder().medications(medicationsList.stream().distinct().collect(Collectors.toList())).build();
-	}
+        return MedicationsResult.builder().medications(medicationsList.stream().distinct().collect(Collectors.toList())).build();
+    }
 
 }
--- src/test/java/com/clearavenue/fdadi/controller/VersionControllerTest.java
+++ src/test/java/com/clearavenue/fdadi/controller/VersionControllerTest.java
@@ -3,11 +3,13 @@ package com.clearavenue.fdadi.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.clearavenue.fdadi.service.VersionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
@@ -24,19 +26,19 @@ public class VersionControllerTest {
 	private MockMvc mockMvc;
 
 	@MockBean
-	BuildProperties buildProperties;
-
-	@MockBean
 	MedicationService medService;
 
 	@MockBean
 	PharmClassService pharmService;
 
 	@MockBean
-	ApiService api;
+	BuildProperties buildProperties;
 
 	@MockBean
-	RestTemplate restTemplate;
+	RestTemplateBuilder rtBuilder;
+
+	@MockBean
+	VersionService versionService;
 
 	@Test
 	public void getVersion() throws Exception {
